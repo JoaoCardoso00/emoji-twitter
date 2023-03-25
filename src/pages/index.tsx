2261661ea30767c224817e42d8ca5cd2f4903846
JoaoCardoso00 <jvcq13@gmail.com>
2023-03-25 15:24:51 -0300

    added posts route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -20,6 +20,11 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
         {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
