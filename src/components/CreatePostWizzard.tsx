975a6a01f1cb6a49ab10ab384bb0d4c216c18f1e
JoaoCardoso00 <jvcq13@gmail.com>
2023-03-26 17:01:22 -0300

    added post creation funcionality

--- src/components/CreatePostWizzard.tsx
+++ src/components/CreatePostWizzard.tsx
@@ -1,8 +1,23 @@
 import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
+import { useState } from "react";
+import { api } from "~/utils/api";
 
 export function CreatePostWizzard() {
   const { user } = useUser();
+  const [input, setInput] = useState("");
+  const { mutate: createPost, isLoading: isPosting } =
+    api.posts.create.useMutation({
+      onSuccess: () => {
+        setInput("");
+        void ctx.posts.getAll.invalidate();
+      },
+    });
+  const ctx = api.useContext();
+
+  function handleCreatePost() {
+    createPost({ content: input });
+  }
 
   if (!user) return null;
 
@@ -19,7 +34,11 @@ export function CreatePostWizzard() {
         type="text"
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={handleCreatePost}>Post</button>
     </div>
   );
 }
